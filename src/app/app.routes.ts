2a5e58e9873cf30a21febe24ab4396ce32a545e5
Erick Valdiviezo <erickvaldiviezo@Laptop-de-Erick.local>
2024-06-18 08:32:09 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './shared/home/home.component';
+import { NosotrosComponent } from './shared/nosotros/nosotros.component';
+import { NoticiasComponent } from './shared/noticias/noticias.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/Home', pathMatch: 'full'},
+    { path: 'Home', component: HomeComponent },
+    { path: 'Nosotros', component: NosotrosComponent },
+    { path: 'Noticias', component: NoticiasComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports:[RouterModule],
+})
+
+export class AppRoutingModule{}
